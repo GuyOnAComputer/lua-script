95f3fe8cd41f5046fd2d7e8f46fc2c444307fc57
HapJim <78453872+HapJim@users.noreply.github.com>
2022-05-19 19:01:44 -0500

    Update mmm_ap_2.lua
    Test

--- mmm_ap_2.lua
+++ mmm_ap_2.lua
@@ -1,46 +1,33 @@
--- [[
-local Discord = "https://discord.gg/QdaJDDvRHN" --[[ join pwease ]]
-local Client = game:GetService'Players'.LocalPlayer
-local MainGui = Client.PlayerGui.ScreenGui.MainGui
+--no skid pls
 
-local Notify = function(Title,Text,Duration)game.StarterGui:SetCore("SendNotification",{Title=Title,Text=Text,Duration=Duration or 1})end
--- ]]
-
--- [[
-
-local uwuware = loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/uwuware-ui/main/main.lua"))() --[[ ip logger ]]
-local Window = uwuware:CreateWindow("CoolUI MMM AP")
-
---#Toggles
+local g = "https://discord.gg/QdaJDDvRHN"
+local Notify=function(Title,Text,Duration)game.StarterGui:SetCore("SendNotification",{Title=Title,Text=Text,Duration=Duration or 1})end --useless XD
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/uwuware-ui/main/main.lua"))() --OMG IP LOGGER!!!!
+local Window = library:CreateWindow("CoolUI MMM AP")
 Window:AddToggle({text = "Toggle autoplayer", flag = "AP" })
-
---#Buttons
 Window:AddButton({text = "Destroy Gui", callback = function()pcall(function()game:GetService("CoreGui").ScreenGui:Destroy()end)end})
-Window:AddButton({text = "Copy discord invite",
-callback=function()
-   if setclipboard then 
-      Notify("Success","Discord invite is in your clipboard")
-      setclipboard(Discord)
-   else
-      Notify("","Exploit doesn't support 'setclipboard', see invite in F9 menu")
-      print("\n\n== DISCORD INVITE ==\n"..Discord.."\n====================")
-   end
+Window:AddButton({text = "Copy discord invite",callback=function()
+if setclipboard then
+    Notify("Success","Discord invite is in your clipboard")
+    setclipboard(g)
+else
+    Notify("","Exploit doesn't support 'setclipboard', see invite in F9 menu")
+    print("\n\n== DISCORD INVITE ==\n" .. g .. "\n====================")
+end
 end})
-
---#Labels
 Window:AddLabel({text = "Autoplayer by lucit#6896"})
 Window:AddLabel({text = "UI and configs by cup#7282"})
 
-uwuware:Init()  --<< initializing ip logger
+library:Init()
 
--- ]]
-
---#Main functions
+local Client = game:GetService'Players'.LocalPlayer
+local MainGui = Client.PlayerGui.ScreenGui.MainGui
 local Background = function()
+  local BG
   for i,v in pairs(MainGui:GetDescendants())do
-    if v.Name == "Background"then return v end
+    if v.Name == "Background"then BG=v end
   end
-  return nil
+  return BG
 end
 local Side = function()
     for _,v in next,Background():GetDescendants() do
@@ -52,32 +39,32 @@ local Side = function()
             end
         end
     end
-    return nil
 end
 local ArrowGui= function()
+  local AG
   for _,v in pairs(MainGui:GetDescendants())do
-    if v.Name == "ArrowGui"then return v end
+    if v.Name == "ArrowGui"then AG=v end
   end
-  return nil
+  return AG
 end
-local FakeContainer=function(_)
-  if ArrowGui() and ArrowGui():FindFirstChild(_) then
-    for i,v in next,ArrowGui()[_]:GetDescendants()do
+local FakeContainer=function(sd)
+  if ArrowGui()~=nil and ArrowGui():FindFirstChild(sd) then
+    for i,v in next,ArrowGui()[sd]:GetDescendants()do
       if v.Name=='FakeContainer'then return v end
     end
-  end
-  return nil
-end
-local ScrollType = function(_)
-  repeat wait() until FakeContainer(_)and #FakeContainer(_):children()>0
-  if FakeContainer(_):children()[1].AbsolutePosition.Y < Client.PlayerGui.ScreenGui.AbsoluteSize.Y/2 then
-      return "Upscroll"
   else
-      return "Downscroll"
+    return nil
   end
-  return nil
 end
-local Init = function(Side)
+local ScrollType = function(Side)
+  repeat wait() until FakeContainer(Side)and #FakeContainer(Side):children()>0
+    if FakeContainer(Side):children()[1].AbsolutePosition.Y < Client.PlayerGui.ScreenGui.AbsoluteSize.Y/2 then
+        return "Upscroll"
+    else
+        return "Downscroll"
+    end
+end
+local Initialize = function(Side)
     repeat wait()until ArrowGui()
     local Arrows = ArrowGui():WaitForChild(Side)
     repeat wait()until #Arrows:WaitForChild'Notes':children()>0
@@ -89,7 +76,7 @@ local Init = function(Side)
         if ScrollType(Side)=="Downscroll"then
             v.ChildAdded:Connect(function(_)
                 repeat task.wait() until _.AbsolutePosition.Y>=Y
-                if library.flags.AP then
+                if library.flags.AP and Keys[_.Parent.Name]~=nil then
                     game:GetService'VirtualInputManager':SendKeyEvent(true,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
                     if #Arrows.LongNotes[_.Parent.Name]:children()==0 then 
                         game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
@@ -124,16 +111,12 @@ local Init = function(Side)
         end
     end
 end
-
---
-
-if ArrowGui()and Background()then
-  Init(Side()) --grabbing btc wallet
-end
-
 MainGui.ChildAdded:Connect(function(_)
     if _.Name == "ArrowGui" then
-        repeat wait() until Background()
-        Init(Side())
+        repeat wait() until ArrowGui()and Background()
+        Initialize(Side())
     end
 end)
+if ArrowGui()and Background()then
+  Initialize(Side())
+end
